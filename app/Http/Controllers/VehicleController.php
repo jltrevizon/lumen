562732d7b48e870d86699ba5580eac293e87195b
Anelvin Mejia <anelvin@reparatucoche.com>
2021-04-07 12:57:16 +0200

    Edit vehicle

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -37,17 +37,17 @@ class VehicleController extends Controller
     public function update(Request $request, $id){
         $vehicle = Vehicle::where('id', $id)
                     ->first();
-        if(isset($request['remote_id'])) $vehicle->remote_id = $request->get('remote_id');
-        if(isset($request['campa_id'])) $vehicle->campa_id = $request->get('campa_id');
-        if(isset($request['category_id'])) $vehicle->category_id = $request->get('category_id');
-        if(isset($request['state_id'])) $vehicle->state_id = $request->get('state_id');
-        if(isset($request['ubication'])) $vehicle->ubication = $request->get('ubication');
-        if(isset($request['plate'])) $vehicle->plate = $request->get('plate');
-        if(isset($request['branch'])) $vehicle->branch = $request->get('branch');
-        if(isset($request['vehicle_model'])) $vehicle->vehicle_model = $request->get('vehicle_model');
-        if(isset($request['version'])) $vehicle->version = $request->get('version');
-        if(isset($request['vin'])) $vehicle->vin = $request->get('vin');
-        if(isset($request['first_plate'])) $vehicle->first_plate = $request->get('first_plate');
+        if($request->json()->get('remote_id')) $vehicle->remote_id = $request->get('remote_id');
+        if($request->json()->get('campa_id')) $vehicle->campa_id = $request->get('campa_id');
+        if($request->json()->get('category_id')) $vehicle->category_id = $request->get('category_id');
+        if($request->json()->get('state_id')) $vehicle->state_id = $request->get('state_id');
+        if($request->json()->get('ubication')) $vehicle->ubication = $request->get('ubication');
+        if($request->json()->get('plate')) $vehicle->plate = $request->get('plate');
+        if($request->json()->get('branch')) $vehicle->branch = $request->get('branch');
+        if($request->json()->get('vehicle_model')) $vehicle->vehicle_model = $request->get('vehicle_model');
+        if($request->json()->get('version')) $vehicle->version = $request->get('version');
+        if($request->json()->get('vin')) $vehicle->vin = $request->get('vin');
+        if($request->json()->get('first_plate')) $vehicle->first_plate = $request->get('first_plate');
         $vehicle->updated_at = date('Y-m-d H:i:s');
         $vehicle->save();
         return $vehicle;
