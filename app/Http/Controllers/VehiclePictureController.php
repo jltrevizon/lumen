5b0b593da9c759e68c9ffc6034462aa6df23ddd4
Anelvin <amejiaroque@gmail.com>
2021-04-08 21:48:25 +0200

    Create picture

--- app/Http/Controllers/VehiclePictureController.php
+++ app/Http/Controllers/VehiclePictureController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\VehiclePicture;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class VehiclePictureController extends Controller
 {
     public function create(Request $request){
         $vehicle_picture = new VehiclePicture();
+        $user = User::where('id', Auth::id())
+                    ->first();
         $vehicle_picture->vehicle_id = $request->json()->get('vehicle_id');
         $vehicle_picture->user_id = Auth::id();
-        $vehicle_picture->campa_id = $request->json()->get('campa_id');
+        $vehicle_picture->campa_id = $user->campa_id;
         $vehicle_picture->url = $request->json()->get('url');
         $vehicle_picture->latitude = $request->json()->get('latitude');
         $vehicle_picture->longitude = $request->json()->get('longitude');
