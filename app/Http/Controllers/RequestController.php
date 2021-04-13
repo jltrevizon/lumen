ff2516b1b4c5d880398faa0595880eec34ee7147
Anelvin Mejia <anelvin@reparatucoche.com>
2021-04-13 09:46:40 +0200

    Return vehicle in requests confirmed

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -80,7 +80,7 @@ class RequestController extends Controller
     }
 
     public function getConfirmedRequest(Request $request){
-        return RequestVehicle::with(['type_request','state_request'])
+        return RequestVehicle::with(['type_request','state_request','vehicle'])
                             ->whereHas('vehicle', function (Builder $builder) use($request){
                                 return $builder->where('campa_id', $request->json()->get('campa_id'));
                             })
