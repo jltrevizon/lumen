4b6f31a780b8892262db4c3b032d5ae25cae3897
Anelvin Mejia <anelvin@reparatucoche.com>
2021-04-28 16:09:34 +0200

    Vehicles available

--- app/Repositories/VehicleRepository.php
+++ app/Repositories/VehicleRepository.php
@@ -286,13 +286,15 @@ class VehicleRepository {
     }
 
     public function getVehiclesAvailableReserveByCampa($request){
-        return Vehicle::where('campa_id', $request->json()->get('campa_id'))
+        return Vehicle::with(['category','campa','state','trade_state'])
+                    ->where('campa_id', $request->json()->get('campa_id'))
                     ->where('trade_state_id', 1)
                     ->get();
     }
 
     public function getVehiclesAvailableReserveByCompany($request){
-        return Vehicle::whereHas('campa', function (Builder $builder) use($request){
+        return Vehicle::with(['category','campa','state','trade_state'])
+                    ->whereHas('campa', function (Builder $builder) use($request){
                         return $builder->where('company_id', $request->json()->get('company_id'));
                     })
                     ->where('trade_state_id', 1)
