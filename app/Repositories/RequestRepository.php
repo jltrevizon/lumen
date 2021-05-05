8bd4a68ae6a23e1d1deeb91be94ef863e15509be
Anelvin Mejia <anelvin@reparatucoche.com>
2021-05-05 15:29:29 +0200

    Bug fix

--- app/Repositories/RequestRepository.php
+++ app/Repositories/RequestRepository.php
@@ -42,11 +42,11 @@ class RequestRepository {
                 $request_vehicle->datetime_request = date('Y-m-d H:i:s');
                 $request_vehicle->save();
                 if($vehicle['type_request_id'] == 2){
-                    $this->taskReservationRepository->create($request_vehicle->id, $request->json()->get('tasks'), $vehicle['vehicle_id'], $request->json()->get('type_reservation_id'));
+                    $this->taskReservationRepository->create($request_vehicle->id, $request->json()->get('tasks'), $vehicle['vehicle_id']);
                     //Estado comercial cambia a solicitado para reserva
                     $this->vehicleRepository->updateTradeState($vehicle['vehicle_id'], 6);
                     //Creamos la reserva con active 0
-                    $this->reservationRepository->create($request_vehicle['id'], $vehicle['vehicle_id'], $request->json()->get('reservation_time'), $request->json()->get('planned_reservation'), $request->json()->get('campa_id'), 0);
+                    $this->reservationRepository->create($request_vehicle['id'], $vehicle['vehicle_id'], $request->json()->get('reservation_time'), $request->json()->get('planned_reservation'), $request->json()->get('campa_id'), 0, $request->json()->get('type_reservation_id'));
                 }
                 array_push($array_request, $request_vehicle);
             }
