fb388e085ec11da14ae312e3e812aa5a395445cb
Anelvin Mejia <anelvin@reparatucoche.com>
2021-05-05 17:06:48 +0200

    bug fix

--- app/Repositories/ReservationRepository.php
+++ app/Repositories/ReservationRepository.php
@@ -68,6 +68,7 @@ class ReservationRepository {
 
     public function getReservationsByVehicle($request){
         return Reservation::where('vehicle_id', $request->json()->get('vehicle_id'))
+                            ->where('active', true)
                             ->orderBy('id', 'desc')
                             ->first();
     }
