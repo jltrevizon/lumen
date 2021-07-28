0b8ec5b6fc5de8297dac6ab9335c2126e36d81de
Anelvin Mejia <anelvin@reparatucoche.com>
2021-07-28 16:30:38 +0200

    Bug fix

--- app/Http/Controllers/Ald/PendingTaskAldController.php
+++ app/Http/Controllers/Ald/PendingTaskAldController.php
@@ -46,12 +46,13 @@ class PendingTaskAldController extends Controller
             $this->vehicleRepository->updateBack($request);
 
             $user = $this->userRepository->getById(Auth::id());
+            return $user;
             $this->vehicleRepository->updateCampa($request->input('vehicle_id'), $user['campas'][0]['id']);
             $reception = $this->receptionRepository->lastReception($request->input('vehicle_id'));
-           /* if($request->input('has_accessories')){
+            if($request->input('has_accessories')){
                 $this->receptionRepository->update($reception['id']);
                 $this->accessoryRepository->create($reception['id'], $request->input('accessories'));
-            }*/
+            }
             return [ 'message' => 'OK' ];
         } catch (Exception $e) {
             return response()->json(['message' => $e->getMessage()], 409);
