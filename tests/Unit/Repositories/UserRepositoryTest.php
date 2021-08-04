8b151fa880d7699ee140babd0b025d60ba6d2caf
Anelvin Mejia <anelvin@reparatucoche.com>
2021-08-04 10:29:23 +0200

    Bug fix

--- tests/Unit/Repositories/UserRepositoryTest.php
+++ tests/Unit/Repositories/UserRepositoryTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use App\Repositories\UserRepository;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
@@ -13,5 +14,16 @@ class UserRepositoryTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->repository = new UserRepository();
+        $this->user = User::factory()->create();
+    }
+
+    /** @test */
+    public function should_return_array_users()
+    {
+        $request = new stdClass();
+        $request->with = [];
+        $users = $this->repository->getAll($request);
+        info($users);
     }
 }
