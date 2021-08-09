cf96eb3c8b0294acb2f157f8e93ca53003e13eeb
Anelvin Mejia <anelvin@reparatucoche.com>
2021-08-09 12:17:56 +0200

    Change order seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Campa;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,30 +15,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //$this->call('UsersTableSeeder');
-        $this->call(AccessorySeeder::class);
+        //$this->call(AccessorySeeder::class);
+        $this->call(RoleSeeder::class);
         $this->call(BrandSeeder::class);
         $this->call(CategorySeeder::class);
-        $this->call(CompanySeeder::class);
-        $this->call(DefleetVariableSeeder::class);
-        $this->call(ProvinceSeeder::class);
-        $this->call(QuestionSeeder::class);
         $this->call(RegionSeeder::class);
+        $this->call(ProvinceSeeder::class);
+        $this->call(CompanySeeder::class);
+        $this->call(CampaSeeder::class);
+        $this->call(DefleetVariableSeeder::class);
+        $this->call(QuestionSeeder::class);
         $this->call(StatePendingTaskSeeder::class);
         $this->call(StateRequestSeeder::class);
         $this->call(StateSeeder::class);
         $this->call(State1Seeder::class);
         $this->call(SubStateSeeder::class);
         $this->call(SubState1Seeder::class);
+        $this->call(OperationTypeSeeder::class);
+        $this->call(TypeTaskSeeder::class);
         $this->call(TaskSeeder::class);
         $this->call(Task1Seeder::class);
         $this->call(TradeStateSeeder::class);
         $this->call(TypeRequestSeeder::class);
         $this->call(TypeReservationSeeder::class);
-        $this->call(TypeTaskSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(VehicleSeeder::class);
         $this->call(TypeUserAppSeeder::class);
-        $this->call(OperationTypeSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(TaxSeeder::class);
+        $this->call(VehicleSeeder::class);
     }
 }
