f58594ffca14e0a6b6ed272956e093049b3b71ad
Anelvin Mejia <anelvin@reparatucoche.com>
2021-08-10 12:52:23 +0200

    Save pending task and return order with vehicle, pending task and workshop

--- app/Repositories/Invarat/InvaratOrderRepository.php
+++ app/Repositories/Invarat/InvaratOrderRepository.php
@@ -2,34 +2,38 @@
 
 namespace App\Repositories\Invarat;
 
-use App\Models\Company;
 use App\Models\Order;
-use App\Models\Role;
 use App\Models\State;
-use App\Models\User;
 use App\Repositories\Repository;
 
 class InvaratOrderRepository extends Repository {
 
     public function __construct(
         InvaratWorkshopRepository $invaratWorkshopRepository,
-        InvaratVehicleRepository $invaratVehicleRepository
+        InvaratVehicleRepository $invaratVehicleRepository,
+        InvaratPendingTaskRepository $invaratPendingTaskRepository
     )
     {
         $this->invaratWorkshopRepository = $invaratWorkshopRepository;
         $this->invaratVehicleRepository = $invaratVehicleRepository;
+        $this->invaratPendingTaskRepository = $invaratPendingTaskRepository;
     }
 
     public function createOrder($request){
         $workshop = $this->invaratWorkshopRepository->createWorkshop($request->input('workshop'));
         $vehicle = $this->invaratVehicleRepository->createVehicle($request);
+        $this->invaratPendingTaskRepository->create($vehicle['id']);
         $order = new Order();
         $order->vehicle_id = $vehicle['id'];
         $order->workshop_id = $workshop['workshop']['id'];
         $order->state_id = State::PENDING_TEST_DINAMIC;
         $order->id_gsp = $request['id_gsp'];
         $order->save();
-        return $order;
+        return Order::with(['workshop','vehicle.pendingTasks' => function ($query) {
+                    return $query->whereNull('datetime_finish');
+                }])
+                ->where('vehicle_id', $vehicle['id'])
+                ->first();
     }
 
     public function orderFilter($request){
---
+++ app/Repositories/Invarat/InvaratPendingTaskRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repositories\Invarat;
+
+use App\Models\Company;
+use App\Models\GroupTask;
+use App\Models\PendingTask;
+use App\Models\StatePendingTask;
+use App\Models\SubState;
+use App\Repositories\GroupTaskRepository;
+use App\Repositories\Repository;
+use App\Repositories\TaskRepository;
+use App\Repositories\UserRepository;
+use App\Repositories\VehicleRepository;
+use Illuminate\Support\Facades\Auth;
+
+class InvaratPendingTaskRepository extends Repository {
+
+    public function __construct(
+        TaskRepository $taskRepository,
+        VehicleRepository $vehicleRepository,
+        GroupTaskRepository $groupTaskRepository
+    )
+    {
+        $this->taskRepository = $taskRepository;
+        $this->vehicleRepository = $vehicleRepository;
+        $this->groupTaskRepository = $groupTaskRepository;
+    }
+
+    public function create($vehicleId){
+        $tasks = $this->taskRepository->getByCompany(Company::INVARAT);
+        $groupTask = $this->groupTaskRepository->createWithVehicleId($vehicleId);
+        $order = 1;
+        foreach($tasks as $task){
+            $pendingTask = new PendingTask();
+            $pendingTask->vehicle_id = $vehicleId;
+            $pendingTask->task_id = $task['id'];
+            $pendingTask->approved = true;
+            if($order == 1) {
+                $pendingTask->state_pending_task_id = StatePendingTask::PENDING;
+                $pendingTask->datetime_pending = date('Y-m-d H:i:s');
+                $this->vehicleRepository->updateSubState($vehicleId, SubState::CAMPA);
+            }
+            $pendingTask->group_task_id = $groupTask['id'];
+            $pendingTask->duration = $task['duration'];
+            $pendingTask->order = $order;
+            $pendingTask->save();
+            $order++;
+        }
+    }
+
+}
