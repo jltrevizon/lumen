3dd587b5ba0cf1b6af656a429a6fbfffcd8ea750
Anelvin Mejia <anelvin@reparatucoche.com>
2021-08-19 09:59:00 +0200

    Change route download vehicles

--- routes/web.php
+++ routes/web.php
@@ -274,7 +274,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
         $router->get('/vehicles/request/defleet', 'VehicleController@vehicleRequestDefleet');
         $router->get('/vehicles/{id}', 'VehicleController@getById');
         $router->post('/vehicles/by-state-date','VehicleController@vehicleByState');
-        $router->get('/vehicles/download/{companyId}', 'VehicleController@download');
+        $router->get('/vehicles/download', 'VehicleController@download');
 
         /**
          * Vehicle Picture
