e2beed0f5efdc2bcdb1a7d244d5de1cea3ef9a9e
Anelvin Mejia <anelvin@reparatucoche.com>
2021-08-25 15:01:43 +0200

    Tests DefleetRepositoryTests

--- app/Repositories/DefleetVariableRepository.php
+++ app/Repositories/DefleetVariableRepository.php
@@ -22,10 +22,10 @@ class DefleetVariableRepository {
 
     public function createVariables($request){
         $user = $this->userRepository->getById($request, Auth::id());
-        $variables = DefleetVariable::byCompany($request, $user['campas'][0]['company_id'])->first();
+        $variables = DefleetVariable::byCompany($user['company_id'])->first();
         if($variables) return [ 'message' => 'Ya existen variables de defleet para esta empresa' ];
         $variables = new DefleetVariable();
-        $variables->company_id = $user['campa']['company_id'];
+        $variables->company_id = $user['company_id'];
         $variables->kms = $request->input('kms');
         $variables->years = $request->input('years');
         $variables->save();
@@ -33,7 +33,8 @@ class DefleetVariableRepository {
     }
 
     public function updateVariables($request){
-        $variables = DefleetVariable::byCompany($request)->first();
+        $user = $this->userRepository->getById($request, Auth::id());
+        $variables = DefleetVariable::byCompany($user['company_id'])->first();
         if(!$variables) return [ 'message' => 'No hay registros que actualizar' ];
         $variables->update($request->all());
         return ['variables' => $variables];
