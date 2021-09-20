fa3899bb4c63ead6656517f3ef5783fe361d30b0
Anelvin Mejia <anelvin@reparatucoche.com>
2021-09-20 12:44:24 +0200

    Observations and add campa_id to receptions

--- app/Repositories/ReceptionRepository.php
+++ app/Repositories/ReceptionRepository.php
@@ -5,16 +5,21 @@ namespace App\Repositories;
 use App\Models\Reception;
 use App\Models\Request;
 use Exception;
+use Illuminate\Support\Facades\Auth;
 
 class ReceptionRepository {
 
-    public function __construct()
+    public function __construct(
+        UserRepository $userRepository
+    )
     {
-
+        $this->userRepository = $userRepository;
     }
 
     public function create($request){
+        $user = $this->userRepository->getById([], Auth::id());
         $reception = new Reception();
+        $reception->campa_id = $user->campas->pluck('id')->toArray()[0];
         $reception->vehicle_id = $request->input('vehicle_id');
         $reception->has_accessories = false;
         $reception->save();
