4a47718538efc8832a13c31dc87cd52febb6bd67
Anelvin <anelvin.mejia@grupomobius.com>
2021-11-03 12:15:47 +0100

    Design report

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -9,25 +9,55 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 </head>
 <body>
-    <h2>Recepciones en la última semana</h2>
-    <img src="https://quickchart.io/chart?c={{$reception_by_days}}" width="600px" alt="">
-    <h2>Recepciones en los últimos meses</h2>
-    <img src="https://quickchart.io/chart?c={{$reception_by_months}}" width="600px" alt="">
-    <h2>Tareas iniciadas en la última semana</h2>
-    <img src="https://quickchart.io/chart?c={{$pending_tasks_by_days}}" width="600px" alt="">
-    <h2>Tareas iniciadas en los últimos meses</h2>
-    <img src="https://quickchart.io/chart?c={{$pending_tasks_by_months}}" width="600px" alt="">
-    <h2>Tareas finalizadas en la última semana</h2>
-    <img src="https://quickchart.io/chart?c={{$pending_tasks_end_by_days}}" width="600px" alt="">
-    <h2>Tareas finalizadas en los últimos meses</h2>
-    <img src="https://quickchart.io/chart?c={{$pending_tasks_end_by_months}}" width="600px" alt="">
-    <h2>Fotos gesionadas en la última semana</h2>
-    <img src="https://quickchart.io/chart?c={{$images_by_days}}" width="600px" alt="">
-    <h2>Fotos gesionadas en la últimos meses</h2>
-    <img src="https://quickchart.io/chart?c={{$images_by_months}}" width="600px" alt="">
-    <h2>Presupuestos de tareas gesionados en la última semana</h2>
-    <img src="https://quickchart.io/chart?c={{$budget_pending_tasks_by_days}}" width="600px" alt="">
-    <h2>Presupuestos de tareas gesionados en la últimos meses</h2>
-    <img src="https://quickchart.io/chart?c={{$budget_pending_tasks_by_months}}" width="600px" alt="">
+    <div style="display: flex; flex-direction:row">
+        <div style="margin-right: 10px">
+            <h2>Recepciones en la última semana</h2>
+            <img src="https://quickchart.io/chart?c={{$reception_by_days}}" width="500px" alt="">
+        </div>
+        <div>
+            <h2>Recepciones en los últimos meses</h2>
+            <img src="https://quickchart.io/chart?c={{$reception_by_months}}" width="500px" alt="">
+        </div>
+    </div>
+    <div style="display: flex; flex-direction:row">
+        <div style="margin-right: 10px">
+            <h2>Tareas iniciadas en la última semana</h2>
+            <img src="https://quickchart.io/chart?c={{$pending_tasks_by_days}}" width="500px" alt="">
+        </div>
+        <div>
+            <h2>Tareas iniciadas en los últimos meses</h2>
+            <img src="https://quickchart.io/chart?c={{$pending_tasks_by_months}}" width="500px" alt="">
+        </div>
+    </div>
+    <div style="display: flex; flex-direction:row">
+        <div style="margin-right: 10px">
+            <h2>Tareas finalizadas en la última semana</h2>
+            <img src="https://quickchart.io/chart?c={{$pending_tasks_end_by_days}}" width="500px" alt="">
+        </div>
+        <div>
+            <h2>Tareas finalizadas en los últimos meses</h2>
+            <img src="https://quickchart.io/chart?c={{$pending_tasks_end_by_months}}" width="500px" alt="">
+        </div>
+    </div>
+    <div style="display: flex; flex-direction:row">
+        <div style="margin-right: 10px">
+            <h2>Fotos gesionadas en la última semana</h2>
+            <img src="https://quickchart.io/chart?c={{$images_by_days}}" width="500px" alt="">
+        </div>
+        <div>
+            <h2>Fotos gesionadas en la últimos meses</h2>
+            <img src="https://quickchart.io/chart?c={{$images_by_months}}" width="500px" alt="">
+        </div>
+    </div>
+    <div style="display: flex; flex-direction:row">
+        <div style="margin-right: 10px">
+            <h2>Presupuestos de tareas gesionados en la última semana</h2>
+            <img src="https://quickchart.io/chart?c={{$budget_pending_tasks_by_days}}" width="500px" alt="">
+        </div>
+        <div>
+            <h2>Presupuestos de tareas gesionados en la últimos meses</h2>
+            <img src="https://quickchart.io/chart?c={{$budget_pending_tasks_by_months}}" width="500px" alt="">
+        </div>
+    </div>
 </body>
 </html>
