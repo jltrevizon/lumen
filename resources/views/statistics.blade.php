192f6cf1f5088705ac2597aaf93a2ad54ce9f3b2
Anelvin <anelvin.mejia@grupomobius.com>
2021-11-03 11:33:35 +0100

    Bug fix

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -9,13 +9,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 </head>
 <body>
-    <h2>Vehículos recepcionados en la última semana</h2>
+    <h2>Recepciones en la última semana</h2>
     <img src="https://quickchart.io/chart?c={{$reception_by_days}}" width="600px" alt="">
-    <h2>Vehículos recepcionados en los últimos meses</h2>
+    <h2>Recepciones en los últimos meses</h2>
     <img src="https://quickchart.io/chart?c={{$reception_by_months}}" width="600px" alt="">
-    <h2>Tareas inciadas en la última semana</h2>
+    <h2>Tareas iniciadas en la última semana</h2>
     <img src="https://quickchart.io/chart?c={{$pending_tasks_by_days}}" width="600px" alt="">
-    <h2>Tareas inciadas en los últimos meses</h2>
+    <h2>Tareas iniciadas en los últimos meses</h2>
     <img src="https://quickchart.io/chart?c={{$pending_tasks_by_months}}" width="600px" alt="">
     <h2>Tareas finalizadas en la última semana</h2>
     <img src="https://quickchart.io/chart?c={{$pending_tasks_end_by_days}}" width="600px" alt="">
