87effb66dd715a1d0e39b1d4e7740146d5bfabcf
Anelvin <anelvin.mejia@grupomobius.com>
2021-11-04 14:09:58 +0100

    Zones seeder

---
+++ database/seeders/ZoneSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Campa;
+use App\Models\Zone;
+use Illuminate\Database\Seeder;
+
+class ZoneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $zones = $this->data();
+        foreach($zones as $zone){
+            Zone::create([
+                'campa_id' => $zone['campa_id'],
+                'name' => $zone['name']
+            ]);
+        }
+    }
+
+    private function data(){
+        return [
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => 'Recepción'
+            ],
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => 'Nave Recepción'
+            ],
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => 'Taller'
+            ],
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => 'Lavadero'
+            ],
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => 'Ubicación'
+            ],
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => '2D'
+            ],
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => 'Entregas'
+            ],
+            [
+                'campa_id' => Campa::LEGANES,
+                'name' => 'Photocall'
+            ],
+        ];
+    }
+}
