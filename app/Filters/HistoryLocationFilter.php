9f7810b634ac4a5388e6d0b930022beee41f2f6f
LeonardoPineda <leo135pineda@gmail.com>
2022-01-12 04:05:55 -0400

    fix

--- app/Filters/HistoryLocationFilter.php
+++ app/Filters/HistoryLocationFilter.php
@@ -31,6 +31,16 @@ class HistoryLocationFilter extends ModelFilter
         return $this->whereIn('user_id', $ids);
     }
 
+    public function createdAt($date){
+        return $this->whereDate('created_at', $date);
+    }
+
+    public function vehiclePlate($plate){
+        return $this->whereHas('vehicle', function(Builder $builder) use($plate){
+            return $builder->where('plate','like',"%$plate%");
+        });
+    }
+
     /**
     * Related Models that have ModelFilters as well as the method on the ModelFilter
     * As [relationMethod => [input_key1, input_key2]].
