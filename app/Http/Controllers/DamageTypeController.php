93ede56242202f31c04fd8a879ba284017143477
Anelvin <anelvin.mejia@grupomobius.com>
2022-02-24 11:50:34 +0100

    Bug fix

--- app/Http/Controllers/DamageTypeController.php
+++ app/Http/Controllers/DamageTypeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use DamageTypeRepository;
+use App\Repositories\DamageTypeRepository;
 use Illuminate\Http\Request;
 
 class DamageTypeController extends Controller
