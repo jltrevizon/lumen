efcb02fc344574ca1fbb0e92f6eb8451c8cf949f
Anelvin <anelvin.mejia@grupomobius.com>
2022-03-01 09:53:03 +0100

    Add softdeletes

--- app/Models/Damage.php
+++ app/Models/Damage.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Damage extends Model
 {
     
-    use HasFactory, Filterable;
+    use HasFactory, Filterable, SoftDeletes;
 
     protected $fillable = [
         'campa_id',
