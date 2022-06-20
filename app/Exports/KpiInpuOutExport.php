9bfaca986f56e9752b5212ed12e31b7b85057147
LeonardoPineda <leo135pineda@gmail.com>
2022-06-20 11:28:01 -0400

    fix

--- app/Exports/KpiInpuOutExport.php
+++ app/Exports/KpiInpuOutExport.php
@@ -2,78 +2,112 @@
 
 namespace App\Exports;
 
-use stdClass;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-
-use App\Models\StatePendingTask;
 use App\Views\KpiView;
-
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\Fromarray;
-use Maatwebsite\Excel\Concerns\FromQuery;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMapping;
 
-class KpiInpuOutExport implements FromCollection, WithMapping, WithHeadings
-{    
-    public function __construct(Request $request)
+class KpiInpuOutExport implements FromArray, WithHeadings
+{
+    public function __construct(int $year)
     {
-        $this->request = $request;
+        $this->year = $year;
     }
 
     public function collection()
     {
+    }
+
+    public function array(): array
+    {
         $in_data = KpiView::with(['typeModelOrder'])
-            ->filter($this->request->all())
+            ->where('in_year', $this->year)
             ->select(
-                DB::raw('count(in_kpi) as `total`'), 
+                DB::raw('count(in_kpi) as `total`'),
                 DB::raw('view_kpis.type_model_order_id'),
                 DB::raw('view_kpis.in_month')
             )
-            ->groupBy('type_model_order_id', 'in_kpi' , 'in_month')
+            ->groupBy('type_model_order_id', 'in_kpi', 'in_month')
             ->get();
+
         $out_data = KpiView::with(['typeModelOrder'])
-            ->filter($this->request->all())
+            ->where('out_year', $this->year)
             ->select(
-                DB::raw('count(out_kpi) as `total`'), 
+                DB::raw('count(out_kpi) as `total`'),
                 DB::raw('view_kpis.type_model_order_id'),
                 DB::raw('view_kpis.out_month')
             )
-            ->groupBy('type_model_order_id', 'out_kpi' ,'out_month')
+            ->groupBy('type_model_order_id', 'out_kpi', 'out_month')
             ->get();
-        return KpiView::all();
-    }
+        $variable = [];
+        foreach ($in_data as $key => $v) {
+            $variable[$v['typeModelOrder']['name']][(int) $v['in_month']] = $v['total'] ?? 0;
+        }
 
-    public function map($data): array
-    {
-        return [
-            $data->id
-        ];
-        /*return [
-           'Bipi', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ];*/
+        $value[] =  ['Entrada', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Optubre', 'Nobiembre', 'Diciembre'];
+
+        foreach ($variable as $key => $v) {
+            $value[] = [
+                $key,
+                $v['1'] ?? 0,
+                $v['2'] ?? 0,
+                $v['3'] ?? 0,
+                $v['4'] ?? 0,
+                $v['5'] ?? 0,
+                $v['6'] ?? 0,
+                $v['7'] ?? 0,
+                $v['8'] ?? 0,
+                $v['9'] ?? 0,
+                $v['10'] ?? 0,
+                $v['11'] ?? 0,
+                $v['12'] ?? 0
+            ];
+        }
+
+        $variable = [];
+        foreach ($out_data as $key => $v) {
+            $variable[$v['typeModelOrder']['name']][(int) $v['out_month']] = $v['total'] ?? 0;
+        }
+
+        $value[] =  ['Salidas', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Optubre', 'Nobiembre', 'Diciembre'];
+
+        foreach ($variable as $key => $v) {
+            $value[] = [
+                $key,
+                $v['1'] ?? 0,
+                $v['2'] ?? 0,
+                $v['3'] ?? 0,
+                $v['4'] ?? 0,
+                $v['5'] ?? 0,
+                $v['6'] ?? 0,
+                $v['7'] ?? 0,
+                $v['8'] ?? 0,
+                $v['9'] ?? 0,
+                $v['10'] ?? 0,
+                $v['11'] ?? 0,
+                $v['12'] ?? 0
+            ];
+        }
+
+        return $value;
     }
 
     public function headings(): array
     {
         return [
-            'ID'
-        ];
-        return [
-           'Negocio',
-           'Enero',
-           'Febrero',
-           'Marzo',
-           'Abril',
-           'Mayo',
-           'Junio',
-           'Julio',
-           'Agosto',
-           'Septiembre',
-           'Optubre',
-           'Noviembre',
-           'Diciembre'
+            'Negocio',
+            'Enero',
+            'Febrero',
+            'Marzo',
+            'Abril',
+            'Mayo',
+            'Junio',
+            'Julio',
+            'Agosto',
+            'Septiembre',
+            'Optubre',
+            'Noviembre',
+            'Diciembre'
         ];
     }
 }
--- app/Http/Controllers/KpiController.php
+++ app/Http/Controllers/KpiController.php
@@ -13,40 +13,46 @@ use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 class KpiController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $data = KpiView::with($this->getWiths($request->with))
             ->filter($request->all())
             ->paginate();
         return $this->getDataResponse($data, HttpFoundationResponse::HTTP_OK);
     }
 
-    public function inpu(Request $request){
+    public function inpu(Request $request)
+    {
         $data = KpiView::with($this->getWiths($request->with))
             ->filter($request->all())
             ->select(
-                DB::raw('count(in_kpi) as `total`'), 
+                DB::raw('count(in_kpi) as `total`'),
                 DB::raw('view_kpis.type_model_order_id'),
                 DB::raw('view_kpis.in_month')
             )
-            ->groupBy('type_model_order_id', 'in_kpi' , 'in_month')
+            ->groupBy('type_model_order_id', 'in_kpi', 'in_month')
             ->get();
+
         return $this->getDataResponse($data, HttpFoundationResponse::HTTP_OK);
     }
 
-    public function out(Request $request){
+    public function out(Request $request)
+    {
         $data = KpiView::with($this->getWiths($request->with))
             ->filter($request->all())
             ->select(
-                DB::raw('count(out_kpi) as `total`'), 
+                DB::raw('count(out_kpi) as `total`'),
                 DB::raw('view_kpis.type_model_order_id'),
                 DB::raw('view_kpis.out_month')
             )
-            ->groupBy('type_model_order_id', 'out_kpi' ,'out_month')
+            ->groupBy('type_model_order_id', 'out_kpi', 'out_month')
             ->get();
         return $this->getDataResponse($data, HttpFoundationResponse::HTTP_OK);
     }
 
-    public function kpiInpuOut(Request $request) {
-        return Excel::download(new KpiInpuOutExport($request), 'salida_entradas-campaa' . date('Y-m-d') . '.xlsx');
+    public function kpiInpuOut(Request $request)
+    {
+        $year = $request->input('year');
+        return Excel::download(new KpiInpuOutExport((int) $year), 'salida_entradas-campaa' . $year . '.xlsx');
     }
 }
