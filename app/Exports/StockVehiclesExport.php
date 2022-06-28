2ce0bbb4c0390584352db500f120e5567ec7874a
LeonardoPineda <leo135pineda@gmail.com>
2022-06-28 09:45:11 -0400

    fix time utc

--- app/Exports/StockVehiclesExport.php
+++ app/Exports/StockVehiclesExport.php
@@ -9,6 +9,7 @@ use App\Models\Vehicle;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Carbon\Carbon;
 
 class StockVehiclesExport implements FromCollection, WithMapping, WithHeadings
 {
@@ -32,32 +33,40 @@ class StockVehiclesExport implements FromCollection, WithMapping, WithHeadings
 
     public function map($vehicle): array
     {
+
         return [
             $vehicle->plate,
-            $vehicle->lastReception ? date('d/m/Y', strtotime($vehicle->lastReception->created_at ?? null)) : null,
+            $vehicle->lastReception ? date('d/m/Y', strtotime($this->fixTime($vehicle->lastReception->created_at ?? null))) : null,
             $vehicle->kms,
             $vehicle->vehicleModel->brand->name ?? null,
             $vehicle->vehicleModel->name ?? null,
             $vehicle->color->name ?? null,
             $vehicle->subState->state->name ?? null,
             $vehicle->subState->name ?? null,
-            $vehicle->last_change_state ? date('d/m/Y H:i:s', strtotime($vehicle->last_change_state)) : null,
-            $vehicle->last_change_sub_state ? date('d/m/Y H:i:s', strtotime($vehicle->last_change_sub_state)) : null,
+            $vehicle->last_change_state ? date('d/m/Y H:i:s', strtotime($this->fixTime($vehicle->last_change_state))) : null,
+            $vehicle->last_change_sub_state ? date('d/m/Y H:i:s', strtotime($this->fixTime($vehicle->last_change_sub_state))) : null,
             $vehicle->observations,
             $vehicle->accessoriesTypeAccessory->pluck('name')->implode(', ') ?? null,
             $vehicle->has_environment_label == true ? 'Si' : 'No',
             $vehicle->campa->name ?? null,
             '',
-            $vehicle->next_itv ? date('d/m/Y', strtotime($vehicle->next_itv)) : null,
+            $vehicle->next_itv ? date('d/m/Y', strtotime($this->fixTime($vehicle->next_itv ?? null))) : null,
             $vehicle->category->name ?? null,
             $vehicle->typeModelOrder->name ?? null,
             $vehicle->lastGroupTask->lastPendingTaskWithState->task->name ?? null,
             $vehicle->lastGroupTask->lastPendingTaskWithState->statePendingTask->name ?? null,
-            $vehicle->lastGroupTask?->lastPendingTaskWithState->datetime_start ? date('d/m/Y', strtotime($vehicle->lastGroupTask?->lastPendingTaskWithState->datetime_start)) : null,
+            $vehicle->lastGroupTask?->lastPendingTaskWithState->datetime_start ? date('d/m/Y', strtotime($this->fixTime($vehicle->lastGroupTask?->lastPendingTaskWithState->datetime_start ?? null))) : null,
             $vehicle->square ? ($vehicle->square->street->zone->name . ' ' . $vehicle->square->street->name . ' ' . $vehicle->square->name) : null,
-            $vehicle->lastDeliveryVehicle ? ($vehicle->sub_state_id == SubState::ALQUILADO ? date('d/m/Y', strtotime($vehicle->lastDeliveryVehicle->created_at)) : null) : null,
+            $vehicle->lastDeliveryVehicle ? ($vehicle->sub_state_id == SubState::ALQUILADO ? date('d/m/Y', strtotime($this->fixTime($vehicle->lastDeliveryVehicle->created_at ?? null))) : null) : null,
             $vehicle->lastGroupTask->lastPendingTaskWithState->observations ?? null
         ];
+    }
+
+    public function fixTime($date) {
+        if ($date) {
+            return (new  Carbon($date))->addHours(2);
+        }
+        return $date;
     }
 
     public function headings(): array
