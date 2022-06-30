51f5c51563125161f05f25b78c2a4c96898c4ee7
rgonzalez <rgonzalez@mkdautomotive.com>
2022-06-30 15:21:35 +0200

    set timeout en pending tasks

--- app/Repositories/PendingTaskRepository.php
+++ app/Repositories/PendingTaskRepository.php
@@ -63,6 +63,7 @@ class PendingTaskRepository extends Repository {
 
     public function pendingTasksFilter($request){
         ini_set("memory_limit", "-1");
+        set_time_limit(60);
         return PendingTask::with($this->getWiths($request->with))
             ->filter($request->all())
             ->whereRaw('vehicle_id NOT IN(SELECT id FROM vehicles WHERE deleted_at is not null)')
