692f7dcac419848f48025ca9d01aa2f7e62baa41
rgonzalez <rgonzalez@mkdautomotive.com>
2022-08-24 11:39:35 +0200

    FC-1118

--- app/Exports/KpiSubStateExport.php
+++ app/Exports/KpiSubStateExport.php
@@ -18,8 +18,10 @@ class KpiSubStateExport implements FromArray
     {
 
         $value[] = ['', '', 'Número de lo que hay en stock', '% de lo que hay en stock'];
+        $value[] = ['Total general', 'Total no disponibles y disponibles', '', '100'];
+        $value[] = ['No Disponible', 'Total de lo que están en estado predisponible + taller + pte venta vo', '', '%'];
 
-        $value[] = ['Total general', 'Total no siponibles y disponibles', '', '100'];
+        /* Taller */
 
         $data = Vehicle::with(['typeModelOrder', 'subState.state'])
             ->filter($this->request->all())
@@ -29,13 +31,13 @@ class KpiSubStateExport implements FromArray
                 DB::raw('sub_state_id')
             )
             ->whereHas('subState', function (Builder $builder) {
-                return $builder->whereIn('state_id', [2, 3, 6]);
+                return $builder->whereIn('state_id', [2]);
             })
             ->whereRaw('id NOT IN(SELECT id FROM vehicles WHERE deleted_at is not null)')
             ->groupBy('type_model_order_id', 'sub_state_id')
             ->get();
 
-        $value[] = ['No Disponible', 'Total de lo que estan en estado predisponible + taller + pte venta vo', '', '%'];
+        $value[] = ['Taller', 'Total de lo que están en estado taller', '', '%'];
         $index = count($value) - 1;
         $total[$index] = 0;
 
@@ -46,7 +48,68 @@ class KpiSubStateExport implements FromArray
         }
 
         $value[$index][2] = $total[$index];
-        $total_no_disponible = $total[$index];
+        $total_taller = $total[$index];
+
+        /* Pendiente V.O. */
+
+        $data = Vehicle::with(['typeModelOrder', 'subState.state'])
+            ->filter($this->request->all())
+            ->select(
+                DB::raw('count(sub_state_id) as `total`'),
+                DB::raw('type_model_order_id'),
+                DB::raw('sub_state_id')
+            )
+            ->whereHas('subState', function (Builder $builder) {
+                return $builder->whereIn('state_id', [3]);
+            })
+            ->whereRaw('id NOT IN(SELECT id FROM vehicles WHERE deleted_at is not null)')
+            ->groupBy('type_model_order_id', 'sub_state_id')
+            ->get();
+
+        $value[] = ['Pendiente Venta V.O.', 'Total de lo que están en estado pendiente de venta vo', '', '%'];
+        $index = count($value) - 1;
+        $total[$index] = 0;
+
+        foreach ($data as $key => $v) {
+            $x =  $v['total'] ?? 0;
+            $total[$index] = $total[$index] + $x;
+            $value[] = [$v['subState']['state']['name'], $v['typeModelOrder']['name'] . ' - ' . $v['subState']['name'], strval($x), '%'];
+        }
+
+        $value[$index][2] = $total[$index];
+        $total_pendiente_venta = $total[$index];
+
+        /* Predisponible */
+
+        $data = Vehicle::with(['typeModelOrder', 'subState.state'])
+            ->filter($this->request->all())
+            ->select(
+                DB::raw('count(sub_state_id) as `total`'),
+                DB::raw('type_model_order_id'),
+                DB::raw('sub_state_id')
+            )
+            ->whereHas('subState', function (Builder $builder) {
+                return $builder->whereIn('state_id', [6]);
+            })
+            ->whereRaw('id NOT IN(SELECT id FROM vehicles WHERE deleted_at is not null)')
+            ->groupBy('type_model_order_id', 'sub_state_id')
+            ->get();
+
+        $value[] = ['Pre-disponible', 'Total de lo que están en estado predisponible', '', '%'];
+        $index = count($value) - 1;
+        $total[$index] = 0;
+
+        foreach ($data as $key => $v) {
+            $x =  $v['total'] ?? 0;
+            $total[$index] = $total[$index] + $x;
+            $value[] = [$v['subState']['state']['name'], $v['typeModelOrder']['name'] . ' - ' . $v['subState']['name'], strval($x), '%'];
+        }
+
+        $value[$index][2] = $total[$index];
+        $total_predisponible = $total[$index];
+
+        
+        /* */        
 
         $data = Vehicle::with(['typeModelOrder', 'subState.state'])
             ->filter($this->request->all())
@@ -78,18 +141,19 @@ class KpiSubStateExport implements FromArray
         $value[$index][2] = $total[$index];
         $total_disponible = $total[$index];
 
-        $acum = $total_no_disponible + $total_disponible;
+        $total_no_disponible = $total_taller + $total_predisponible + $total_pendiente_venta;
         /*
         $acum = 0;
         for ($i = 0; $i < count($total); $i++) {
             $acum += $total[$i] ?? 0;
         }
         */
-        $value[1][2] = strval($acum);
+        $value[1][2] = strval($total_general);
+        $value[2][2] = strval($total_no_disponible);
         
         for ($i = 0; $i < count($value); $i++) {
             if ($value[$i][3] == '%') {
-                $value[$i][3] = $this->obtenerPorcentaje((int) $value[$i][2], $acum);
+                $value[$i][3] = $this->obtenerPorcentaje((int) $value[$i][2], $total_general);
             }
         }
 
