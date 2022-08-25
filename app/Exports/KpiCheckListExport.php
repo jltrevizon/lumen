670b566c1d9e9ef25eeb0c634e55c1ad7b1421bd
rgonzalez <rgonzalez@mkdautomotive.com>
2022-08-25 10:58:47 +0200

    FC-1117

--- app/Exports/KpiCheckListExport.php
+++ app/Exports/KpiCheckListExport.php
@@ -17,6 +17,24 @@ class KpiCheckListExport implements FromArray, WithHeadings
 
     public function array(): array
     {
+
+        $data = Vehicle::with(['subState.state'])
+            ->select(
+                DB::raw('count(*) vehiculos'),
+                DB::raw('sum((select count(*) from pending_tasks where vehicle_id = vehicles.id and task_id = 2 and approved = 1 and (state_pending_task_id in (1, 2) or (state_pending_task_id is null and comment_state is null)) )) chapa'),
+                DB::raw('sum((select count(*) from pending_tasks where vehicle_id = vehicles.id and task_id = 3 and approved = 1 and (state_pending_task_id in (1, 2) or (state_pending_task_id is null and comment_state is null)) )) mecanica')
+            )
+            ->whereHas('subState', function (Builder $builder) {
+                return $builder->whereIn('state_id', [11]);
+            })
+            ->whereRaw('id NOT IN(SELECT id FROM vehicles WHERE deleted_at is not null)')
+            ->get();
+
+            $value[] = [$data[0]['vehiculos'], $data[0]['chapa'], $data[0]['mecanica']];
+
+            return $value;
+
+/*
         $year = $this->request->input('year') ?? date('Y');
         $data = GroupTask::with(['typeModelOrder'])
             ->filter($this->request->all())
@@ -116,6 +134,7 @@ class KpiCheckListExport implements FromArray, WithHeadings
             ];
         }
         return $value;
+*/
     }
 
     public function obtenerPorcentaje($cantidad, $total)
