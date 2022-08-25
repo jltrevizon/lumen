1508d05f7fbfe73ecfb385a550ada23f135a09a0
rgonzalez <rgonzalez@mkdautomotive.com>
2022-08-25 15:11:27 +0200

    FC-1117

--- app/Exports/KpiCheckListExport.php
+++ app/Exports/KpiCheckListExport.php
@@ -20,6 +20,19 @@ class KpiCheckListExport implements FromArray, WithHeadings
     public function array(): array
     {
 
+        $data = Vehicle::whereHas('lastUnapprovedGroupTask')
+            ->whereHas('campa', function (Builder $builder) {
+                return $builder->where('company_id', Company::ALD);
+            })
+            ->select(
+                DB::raw('count(*) vehiculos'),
+                DB::raw('sum((select count(*) from pending_tasks where vehicle_id = vehicles.id and task_id = 2 and approved = 1 and (state_pending_task_id in (1, 2) or (state_pending_task_id is null and comment_state is null)) )) chapa'),
+                DB::raw('sum((select count(*) from pending_tasks where vehicle_id = vehicles.id and task_id = 3 and approved = 1 and (state_pending_task_id in (1, 2) or (state_pending_task_id is null and comment_state is null)) )) mecanica')
+            )
+            // ->filter($request->all())
+            ->get();
+
+/*
         $data = Vehicle::with(['subState.state'])
             ->select(
                 DB::raw('count(*) vehiculos'),
@@ -29,7 +42,7 @@ class KpiCheckListExport implements FromArray, WithHeadings
             ->whereRaw('sub_state_id = 11')
             ->whereRaw('id NOT IN(SELECT id FROM vehicles WHERE deleted_at is not null)')
             ->get();
-
+*/
             $value[] = ['Checklist pendientes', 'Checklist pendientes (vehículos en estado pendiente check) totales.', strval($data[0]['vehiculos'])];
             $value[] = ['Mecánica', 'Número de tareas de mecánica de vehículos pendiente check', strval($data[0]['mecanica'])];
             $value[] = ['Chapa', 'Número de tareas de chapa de vehiculos pendiente check.', strval($data[0]['chapa'])];
