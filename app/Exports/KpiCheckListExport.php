4aadd939b8c67b603983ce11d1833d445fa73d2a
rgonzalez <rgonzalez@mkdautomotive.com>
2022-08-25 13:27:53 +0200

    FC-1117

--- app/Exports/KpiCheckListExport.php
+++ app/Exports/KpiCheckListExport.php
@@ -21,13 +21,11 @@ class KpiCheckListExport implements FromArray, WithHeadings
     {
 
         $data = Vehicle::with(['subState.state'])
-            /*
             ->select(
-                DB::raw('count(*) vehiculos'),
+                // DB::raw('count(*) vehiculos'),
                 DB::raw('sum((select count(*) from pending_tasks where vehicle_id = vehicles.id and task_id = 2 and approved = 1 and (state_pending_task_id in (1, 2) or (state_pending_task_id is null and comment_state is null)) )) chapa'),
                 DB::raw('sum((select count(*) from pending_tasks where vehicle_id = vehicles.id and task_id = 3 and approved = 1 and (state_pending_task_id in (1, 2) or (state_pending_task_id is null and comment_state is null)) )) mecanica')
             )
-            */
             ->whereRaw('sub_state_id = 11')
             ->whereRaw('id NOT IN(SELECT id FROM vehicles WHERE deleted_at is not null)')
             ->get();
@@ -37,7 +35,7 @@ class KpiCheckListExport implements FromArray, WithHeadings
             $value[] = ['Chapa', strval(count($data)), '2', '', '', '', '', '', '', '', '', '', ''];
             // $value[] = [$data[0]['vehiculos'], $data[0]['chapa'], $data[0]['mecanica']];
             foreach ($data as $key => $v) {
-                $value[] = [strval($v['id'])];
+                $value[] = [strval($v['id']), strval($v['mecanica'], strval($v['chapa']))];
             }
 
             return $value;
