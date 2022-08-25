40ca8e1b066e933eb673b46b1c30450596f09424
rgonzalez <rgonzalez@mkdautomotive.com>
2022-08-25 13:37:38 +0200

    FC-1117

--- app/Exports/KpiCheckListExport.php
+++ app/Exports/KpiCheckListExport.php
@@ -35,7 +35,11 @@ class KpiCheckListExport implements FromArray, WithHeadings
             $value[] = ['Chapa', strval(count($data)), '2', '', '', '', '', '', '', '', '', '', ''];
             // $value[] = [$data[0]['vehiculos'], $data[0]['chapa'], $data[0]['mecanica']];
             foreach ($data as $key => $v) {
-                $value[] = [strval($v['id']), strval($v['mecanica']), strval($v['chapa']))];
+                $value[] = [
+                    strval($v['id']), 
+                    strval($v['mecanica']), 
+                    strval($v['chapa'])
+                ];
             }
 
             return $value;
