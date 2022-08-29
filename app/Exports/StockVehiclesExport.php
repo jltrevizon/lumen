390eb543b0e5063956c4b413947d36dae7de4d29
LeonardoPineda <leo135pineda@gmail.com>
2022-08-29 13:02:06 -0400

    fix

--- app/Exports/StockVehiclesExport.php
+++ app/Exports/StockVehiclesExport.php
@@ -38,26 +38,26 @@ class StockVehiclesExport implements FromCollection, WithMapping, WithHeadings
         $pendingTask = $vehicle->lastGroupTask?->lastPendingTaskWithState;
         return [
             $vehicle->plate,
-            $vehicle->lastReception ? date('d/m/Y', strtotime($this->fixTime($vehicle->lastReception->created_at ?? null))) : null,
+            $vehicle->lastReception ? $this->fixTime($vehicle->lastReception->created_at ?? null) : null,
             $vehicle->kms,
             $vehicle->vehicleModel->brand->name ?? null,
             $vehicle->vehicleModel->name ?? null,
             $vehicle->color->name ?? null,
             $vehicle->subState->state->name ?? null,
             $vehicle->subState->name ?? null,
-            $pendingTask?->last_change_state ? date('d/m/Y H:i:s', strtotime($this->fixTime($vehicle->last_change_state))) : null,
-            $pendingTask?->last_change_sub_state ? date('d/m/Y H:i:s', strtotime($this->fixTime($vehicle->last_change_sub_state))) : null,
+            $pendingTask?->last_change_state ? $this->fixTime($vehicle->last_change_state) : null,
+            $pendingTask?->last_change_sub_state ? $this->fixTime($vehicle->last_change_sub_state) : null,
             $vehicle->observations,
             $vehicle->accessoriesTypeAccessory->pluck('name')->implode(', ') ?? null,
             $vehicle->has_environment_label == true ? 'Si' : 'No',
             $vehicle->campa->name ?? null,
             '',
-            $vehicle->next_itv ? date('d/m/Y', strtotime($this->fixTime($vehicle->next_itv ?? null))) : null,
+            $vehicle->next_itv ? $this->fixTime($vehicle->next_itv ?? null) : null,
             $vehicle->category->name ?? null,
             $vehicle->typeModelOrder->name ?? null,
             $pendingTask->task->name ?? null,
             $pendingTask->statePendingTask->name ?? null,
-            $pendingTask?->datetime_start ? date('d/m/Y', strtotime($this->fixTime($vehicle->lastGroupTask?->lastPendingTaskWithState->datetime_start ?? null))) : null,
+            $pendingTask?->datetime_start ? $this->fixTime($vehicle->lastGroupTask?->lastPendingTaskWithState->datetime_start ?? null) : null,
             $vehicle->square && $vehicle->square->street && $vehicle->square->street->zone ? ($vehicle->square->street->zone->name . ' ' . $vehicle->square->street->name . ' ' . $vehicle->square->name) : null,
             // $vehicle->lastDeliveryVehicle ? ($vehicle->sub_state_id == SubState::ALQUILADO ? date('d/m/Y', strtotime($this->fixTime($vehicle->lastDeliveryVehicle->created_at ?? null))) : null) : null,
             $pendingTask?->observations ?? null
@@ -66,7 +66,7 @@ class StockVehiclesExport implements FromCollection, WithMapping, WithHeadings
 
     public function fixTime($date) {
         if ($date) {
-            return (new  Carbon($date))->addHours(2);
+            return (new  Carbon($date))->addHours(2)->format('d/m/Y H:m:i');
         }
         return $date;
     }
