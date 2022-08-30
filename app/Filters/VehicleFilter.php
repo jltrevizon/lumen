cf23c6464cfff9a597eafea9e7428c078ab41aab
LeonardoPineda <leo135pineda@gmail.com>
2022-08-30 12:32:04 -0400

    fix

--- app/Filters/VehicleFilter.php
+++ app/Filters/VehicleFilter.php
@@ -22,13 +22,11 @@ class VehicleFilter extends ModelFilter
         return $this->campasIds($ids);
     }
 
-    public function companyIds($ids)
-    {
+    public function companyIds($ids){
         return $this->byCompanies($ids);
     }
 
-    public function createdAt($date)
-    {
+    public function createdAt($date){
         return $this->whereDate('created_at', $date);
     }
 
@@ -52,23 +50,20 @@ class VehicleFilter extends ModelFilter
         return $this->brandIds($ids);
     }
 
-    public function typeModelOrderIds($ids)
-    {
+    public function typeModelOrderIds($ids){
         return $this->byTypeModelOrderIds($ids);
     }
 
-    public function budgetPendingTaskIds($ids)
-    {
+    public function budgetPendingTaskIds($ids){
         return $this->byBudgetPendingTaskIds($ids);
     }
 
-    public function budgetLastGroupTaskIds($ids)
-    {
-        return $this->whereHas('lastGroupTask.pendingTasks', function (Builder $builder) use ($ids) {
-            return $builder->whereHas('budgetPendingTasks', function ($query) use ($ids) {
+    public function budgetLastGroupTaskIds($ids){
+        return $this->whereHas('lastGroupTask.pendingTasks', function(Builder $builder) use($ids){
+            return $builder->whereHas('budgetPendingTasks', function ($query) use($ids){
                 return $query->whereIn('state_budget_pending_task_id', $ids);
             })
-                ->where('approved', true);
+            ->where('approved', true);
         });
     }
 
@@ -82,9 +77,8 @@ class VehicleFilter extends ModelFilter
         return $this->categoriesIds($ids);
     }
 
-    public function groupTaskIds($ids)
-    {
-        return $this->whereHas('groupTasks', function (Builder $builder) use ($ids) {
+    public function groupTaskIds($ids){
+        return $this->whereHas('groupTasks', function(Builder $builder) use($ids){
             return $builder->whereIn('id', $ids);
         });
     }
@@ -104,138 +98,124 @@ class VehicleFilter extends ModelFilter
         return $this->byReadyDelivery($value);
     }
 
-    public function statePendingTasks($ids)
-    {
+    public function statePendingTasks($ids){
         return $this->byStatePendingTasks($ids);
     }
 
-    public function subStatePendingTasks($ids)
-    {
+    public function subStatePendingTasks($ids){
         return $this->bySubStatePendingTasks($ids);
     }
 
-    public function subStateNull()
-    {
+    public function subStateNull(){
         return $this->bySubStateNull();
     }
 
-    public function whereHasBudgetPendingTask()
-    {
+    public function whereHasBudgetPendingTask(){
         return $this->byWhereHasBudgetPendingTask();
     }
 
-    public function byUserRole($roleId)
-    {
+    public function byUserRole($roleId){
         return $this->byRole($roleId);
     }
 
-    public function hasGroupTaskUnapproved($value)
-    {
+    public function hasGroupTaskUnapproved($value){
         return $this->byHasGroupTaskUnapproved($value);
     }
 
-    public function hasOrderNotFinish($value)
-    {
+    public function hasOrderNotFinish($value){
         return $this->byHasOrderNotFinish($value);
     }
 
-    public function hasOrdersStateIds($value)
-    {
+    public function hasOrdersStateIds($value){
         return $this->byHasOrdersStateIds($value);
     }
 
-    public function taskIds($ids)
-    {
+    public function taskIds($ids){
         return $this->byTaskIds($ids);
     }
 
-    public function taskSubStatesIds($ids)
-    {
+    public function taskSubStatesIds($ids){
         return $this->byTaskSubStatesIds($ids);
     }
 
-    public function taskStatesIds($ids)
-    {
+    public function taskStatesIds($ids){
         return $this->byTaskStatesIds($ids);
     }
 
-    public function whereHasBudgetPendingTaskByState($stateId)
-    {
+    public function whereHasBudgetPendingTaskByState($stateId){
         return $this->whereHas('pendingTask.budgetPendingTasks', fn (Builder $builder) => $builder->where('state_budget_pending_task_id', $stateId));
     }
 
     public function pendingTaskDateTimeStartFrom($dateTime)
     {
-        return $this->whereHas('pendingTasks', function ($query) use ($dateTime) {
-            return $query->whereDate('datetime_start', '>=', $dateTime);
+        return $this->whereHas('pendingTasks', function($query) use ($dateTime) {
+            return $query->whereDate('datetime_start','>=', $dateTime); 
         });
     }
 
     public function pendingTaskDateTimeStartTo($dateTime)
     {
-        return $this->whereHas('pendingTasks', function ($query) use ($dateTime) {
-            return $query->whereDate('datetime_start', '<=', $dateTime);
+        return $this->whereHas('pendingTasks', function($query) use ($dateTime) {
+            return $query->whereDate('datetime_start','<=', $dateTime); 
         });
     }
 
     public function pendingTaskDateTimeEndFrom($dateTime)
     {
-        return $this->whereHas('pendingTasks', function ($query) use ($dateTime) {
-            return $query->whereDate('datetime_end', '>=', $dateTime);
+        return $this->whereHas('pendingTasks', function($query) use ($dateTime) {
+            return $query->whereDate('datetime_end','>=', $dateTime); 
         });
     }
 
     public function pendingTaskDateTimeEndTo($dateTime)
     {
-        return $this->whereHas('pendingTasks', function ($query) use ($dateTime) {
-            return $query->whereDate('datetime_end', '<=', $dateTime);
+        return $this->whereHas('pendingTasks', function($query) use ($dateTime) {
+            return $query->whereDate('datetime_end','<=', $dateTime); 
         });
     }
 
     public function pendingTaskDateTimePendingFrom($dateTime)
     {
-        return $this->whereHas('pendingTasks', function ($query) use ($dateTime) {
-            return $query->whereDate('datetime_pending', '>=', $dateTime);
+        return $this->whereHas('pendingTasks', function($query) use ($dateTime) {
+            return $query->whereDate('datetime_pending','>=', $dateTime); 
         });
     }
 
     public function pendingTaskDateTimePendingTo($dateTime)
     {
-        return $this->whereHas('pendingTasks', function ($query) use ($dateTime) {
-            return $query->whereDate('datetime_pending', '<=', $dateTime);
+        return $this->whereHas('pendingTasks', function($query) use ($dateTime) {
+            return $query->whereDate('datetime_pending','<=', $dateTime); 
         });
     }
 
     public function lastReceptionCreatedAtFrom($dateTime)
     {
-        return $this->whereHas('lastReception', function ($query) use ($dateTime) {
-            return $query->whereDate('created_at', '>=', $dateTime);
+        return $this->whereHas('lastReception', function($query) use ($dateTime) {
+            return $query->whereDate('created_at', '>=',$dateTime); 
         });
     }
 
     public function lastReceptionCreatedAtTo($dateTime)
     {
-        return $this->whereHas('lastReception', function ($query) use ($dateTime) {
-            return $query->whereDate('created_at', '<=', $dateTime);
+        return $this->whereHas('lastReception', function($query) use ($dateTime) {
+            return $query->whereDate('created_at', '<=',$dateTime); 
         });
     }
 
     public function lastReceptionCreatedAt($dateTime)
     {
-        return $this->whereHas('lastReception', function ($query) use ($dateTime) {
-            return $query->whereDate('created_at', $dateTime);
+        return $this->whereHas('lastReception', function($query) use ($dateTime) {
+            return $query->whereDate('created_at', $dateTime); 
         });
     }
 
     public function approvedPendingTasksNotNull($value)
     {
         if ($value) {
-            $vehicle = Vehicle::whereHas('lastGroupTask', function ($query) {
+            $vehicle = Vehicle::whereHas('lastGroupTask', function($query) { 
                 return $query->whereDoesntHave('approvedPendingTasks');
             })->get('id');
-            $value = collect($vehicle)->map(function ($item) {
-                return $item->id;
-            })->toArray();
+            $value = collect($vehicle)->map(function ($item){ return $item->id;})->toArray();
             return $this->whereNotIn('id', $value);
         }
     }
@@ -252,51 +232,44 @@ class VehicleFilter extends ModelFilter
                 ORDER BY pt.state_pending_task_id DESC, pt.order, pt.datetime_finish DESC 
                 limit 1
             SQL;
-            return $this->selectRaw('*,(' . $sql . ') as task_id')
-                ->whereRaw('(' . $sql . ') IN(' . implode(',', $value) . ')');
+           return $this->selectRaw('*,('. $sql .') as task_id')
+           ->whereRaw('('. $sql .') IN('. implode(',', $value) .')');
         }
     }
 
     public function isDefleeting($value)
     {
-        if ($value) {
+        if($value){
             return $this->whereHas('lastReception.groupTask', fn ($builder) => $builder->whereNotNull('datetime_defleeting'));
         } else {
             return $this->whereHas('lastReception.groupTask', fn ($builder) => $builder->whereNull('datetime_defleeting'));
         }
     }
 
-    public function defleetingAndDelivery($value)
-    {
+    public function defleetingAndDelivery($value) {
         $vehicle_ids = collect(
             PendingTask::where('state_pending_task_id', 3)
-                ->where('approved', 1)
-                ->where('task_id', 38)
-                ->whereRaw(Db::raw('vehicle_id in (SELECT v.id from vehicles v where v.sub_state_id = 8)'))
-                ->get('vehicle_id')
-        )->map(function ($item) {
-            return $item->vehicle_id;
-        })->toArray();
-        if (count($vehicle_ids) > 0) {
-            if ($value) {
-                return $this->whereNotIn('id', $vehicle_ids);
-            }
-            return $this->whereIn('id', $vehicle_ids);
+            ->where('approved', 1)
+            ->where('task_id', 38)
+            ->whereRaw(Db::raw('vehicle_id in (SELECT v.id from vehicles v where v.sub_state_id = 8)'))
+            ->get('vehicle_id')
+            )->map(function ($item){ return $item->vehicle_id;})->toArray();
+        if ($value == 1) {
+            return $this->whereNotIn('id', $vehicle_ids);
         }
+        return $this->whereIn('id', $vehicle_ids);
     }
 
-    public function hasDamage($value)
-    {
+    public function hasDamage($value){
         return $this->whereHas('lastGroupTask.damages');
     }
 
-    public function orderDesc($field)
-    {
+    public function orderDesc($field){
         return $this->orderByDesc($field);
     }
 
-    public function order($field)
-    {
+    public function order($field){
         return $this->orderBy($field);
     }
+
 }
