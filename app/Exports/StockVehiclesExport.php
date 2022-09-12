477d125faa1fcefef5b74e8f9fe7c039250807cd
rgonzalez <rgonzalez@mkdautomotive.com>
2022-09-12 19:33:26 +0200

    add sub_state is null to stock

--- app/Exports/StockVehiclesExport.php
+++ app/Exports/StockVehiclesExport.php
@@ -22,10 +22,15 @@ class StockVehiclesExport implements FromCollection, WithMapping, WithHeadings
     public function collection()
     {
         if($this->campaId == 3) {
-            return Vehicle::whereRaw('((sub_state_id is null and campa_id = '.$this->campaId.') or sub_state_id != '.SubState::ALQUILADO.')')
-                // where('sub_state_id', '!=', SubState::ALQUILADO)
+
+            return Vehicle::where('sub_state_id', '!=', SubState::ALQUILADO)
                 ->filter([ 'defleetingAndDelivery' => 1 ])
                 ->get();
+            /*
+            return Vehicle::whereRaw('((sub_state_id is null and campa_id = '.$this->campaId.') or sub_state_id != '.SubState::ALQUILADO.')')
+                ->filter([ 'defleetingAndDelivery' => 1 ])
+                ->get();
+            */
         } if($this->campaId == null) {
             return Vehicle::where('sub_state_id', '!=', SubState::ALQUILADO)
             ->filter([ 'defleetingAndDelivery' => 1 ])
