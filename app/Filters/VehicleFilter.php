2dc305dacbaf5c169864cbfca0ee555458193190
rgonzalez <rgonzalez@mkdautomotive.com>
2022-09-15 13:25:06 +0200

    test sub state null

--- app/Filters/VehicleFilter.php
+++ app/Filters/VehicleFilter.php
@@ -266,8 +266,13 @@ class VehicleFilter extends ModelFilter
                 ->where('campa_id', '3')
                 ->whereNull('deleted_at');
         }
-        
     }
+
+    public function testSubStateNull($value) {
+        return $this->orWhereNull('sub_state_id');
+    }
+
+    
 
     public function hasDamage($value){
         return $this->whereHas('lastGroupTask.damages');
