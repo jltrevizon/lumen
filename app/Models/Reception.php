da00ca807c089ff4775a8519baac5407feebe38c
LeonardoPineda <leo135pineda@gmail.com>
2022-10-04 07:11:55 -0400

    fix

--- app/Models/Reception.php
+++ app/Models/Reception.php
@@ -53,7 +53,11 @@ class Reception extends Model
     }
 
     public function defleetingAndDelivery($value) {
-        $vehicle_ids = collect(PendingTask::where('state_pending_task_id', 3)->where('task_id', 38)->whereRaw(DB::raw('vehicle_id in (SELECT v.id from vehicles v where v.sub_state_id = 8)'))->get('vehicle_id'))->map(function ($item){ return $item->vehicle_id;})->toArray();
+        $vehicle_ids = collect(PendingTask::where('state_pending_task_id', 3)
+            ->where('task_id', 38)
+            ->whereRaw(Db::raw('reception_id = (Select max(r.id) from receptions r where r.vehicle_id = pending_tasks.vehicle_id)'))
+            ->whereRaw(DB::raw('vehicle_id in (SELECT v.id from vehicles v where v.sub_state_id = 8)'))
+            ->get('vehicle_id'))->map(function ($item){ return $item->vehicle_id;})->toArray();
         if ($value) {
             return $this->whereNotIn('vehicle_id', $vehicle_ids);
         }
