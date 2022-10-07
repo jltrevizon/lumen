9ddf2e4cfeaee0947d1aadd579822469a183f06f
LeonardoPineda <leo135pineda@gmail.com>
2022-10-07 09:57:22 -0400

    fix vehicle pictures

--- app/Repositories/VehiclePictureRepository.php
+++ app/Repositories/VehiclePictureRepository.php
@@ -5,17 +5,20 @@ namespace App\Repositories;
 use App\Models\Vehicle;
 use App\Models\VehiclePicture;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
-class VehiclePictureRepository extends Repository {
+class VehiclePictureRepository extends Repository
+{
 
     public function __construct(UserRepository $userRepository)
     {
         $this->userRepository = $userRepository;
     }
 
-    public function create($data){
+    public function create($data)
+    {
         $vehicle = Vehicle::with(['lastReception'])
-                    ->findOrFail($data['vehicle_id']);
+            ->findOrFail($data['vehicle_id']);
         $vehicle_picture = new VehiclePicture();
         $vehicle_picture->reception_id = $vehicle['lastReception']['id'] ?? null;
         $vehicle_picture->vehicle_id = $vehicle->id;
@@ -26,16 +29,19 @@ class VehiclePictureRepository extends Repository {
         $vehicle_picture->longitude = $data['longitude'];
         $vehicle_picture->save();
         return VehiclePicture::where('vehicle_id', $vehicle->id)->where('reception_id', $vehicle_picture->reception_id)
-                ->get();
+            ->get();
     }
 
-    public function getPicturesByVehicle($request){
+    public function getPicturesByVehicle($request)
+    {
         return VehiclePicture::with($this->getWiths($request->with))
-                    ->where('vehicle_id', $request->input('vehicle_id'))
-                    ->get();
+            ->whereRaw(DB::raw('reception_id = (Select max(r.id) from receptions r where r.vehicle_id = vehicle_pictures.vehicle_id)'))
+            ->where('vehicle_id', $request->input('vehicle_id'))
+            ->get();
     }
 
-    public function deletePictureByReception($reception){
+    public function deletePictureByReception($reception)
+    {
         VehiclePicture::where('vehicle_id', $reception['id'])
             ->delete();
         return ['message' => 'Pictures deleted'];
@@ -45,37 +51,34 @@ class VehiclePictureRepository extends Repository {
      * Método para borrar imagenes de un determinado vehículo y lugar
      *
      */
-    public function deletePictureByPlace($request){
+    public function deletePictureByPlace($request)
+    {
         $pictures = VehiclePicture::where('vehicle_id', $request->input['vehicle_id'])
             ->where('place', $request->input['place'])
             ->get();
 
-        if($pictures){
+        if ($pictures) {
 
-            foreach($pictures as $pic){
+            foreach ($pictures as $pic) {
                 $pic->delete();
             }
 
             return ['message' => 'Pictures deleted'];
-        }else{
+        } else {
             return ['message' => 'Picture not found'];
         }
-
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
 
         $pic = VehiclePicture::find($id);
 
-        if($pic){
+        if ($pic) {
             $pic->delete();
             return ['message' => 'Picture deleted'];
-        }else{
+        } else {
             return ['message' => 'Picture not found'];
         }
-
     }
-
-
-
 }
