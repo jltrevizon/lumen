907299ae2b4d326b5ffc1fc3654fa975e83a551a
LeonardoPineda <leo135pineda@gmail.com>
2022-11-08 20:52:22 -0400

    fix group task and receptions

--- app/Filters/VehicleFilter.php
+++ app/Filters/VehicleFilter.php
@@ -265,6 +265,7 @@ class VehicleFilter extends ModelFilter
                 select pt.task_id
                 from pending_tasks pt
                 WHERE pt.state_pending_task_id in (1, 2)
+                AND pt.reception_id = (Select max(r.id) from receptions r where r.vehicle_id = pt.vehicle_id)
                 AND pt.approved = 1
                 AND pt.vehicle_id = vehicles.id
                 ORDER BY pt.state_pending_task_id DESC, pt.order, pt.datetime_finish DESC
